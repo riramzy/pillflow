bd12393baa6ab52558231bedf3d0868a4ba047e3
riramzy <ramzyi081@gmail.com>
2026-07-16 20:40:36 +0300

    Cleanup template boilerplate and update app icons
    - Remove `Greeting` logic and unused resources from the shared module.
    - Refactor `App.kt` to use `PillFlowTheme` and remove placeholder UI.
    - Update Android adaptive icons to use color backgrounds and add monochrome support.
    - Remove legacy raster launcher icons (PNGs) for Android.
    - Update iOS app icon assets.

--- shared/src/commonMain/kotlin/com/riramzy/pillfllow/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.riramzy.pillfllow
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return sayHello(platform.name)
-    }
-}
--- shared/src/commonMain/kotlin/com/riramzy/pillfllow/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package com.riramzy.pillfllow
-
-fun sayHello(to: String): String =
-    "Hello, $to!"
