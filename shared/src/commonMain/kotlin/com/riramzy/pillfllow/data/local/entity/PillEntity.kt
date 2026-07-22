f0912b3f5273f4731a9aa914f1adfa5ec6c6721f
riramzy <ramzyi081@gmail.com>
2026-07-22 21:42:48 +0300

    Update `PillEntity` to use Compose Color and add shape property
    - Replace `colorHex` string with `androidx.compose.ui.graphics.Color` for better UI integration.
    - Add `shape` property using `PillShape` with a default value of `CIRCLE`.

--- shared/src/commonMain/kotlin/com/riramzy/pillfllow/data/local/entity/PillEntity.kt
+++ shared/src/commonMain/kotlin/com/riramzy/pillfllow/data/local/entity/PillEntity.kt
@@ -1,11 +1,14 @@
 package com.riramzy.pillfllow.data.local.entity
 
+import androidx.compose.ui.graphics.Color
 import com.riramzy.pillfllow.domain.physics.Vector2D
+import com.riramzy.pillfllow.utils.PillShape
 
 data class PillEntity(
     val id: String,
     val name: String,
-    val colorHex: String,
+    val color: Color,
+    val shape: PillShape = PillShape.CIRCLE,
     val radius: Float,
     var position: Vector2D,
     var velocity: Vector2D = Vector2D(0f, 0f)
