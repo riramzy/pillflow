dacbd672c575548f2c1e9564fff9e955d765824f
riramzy <ramzyi081@gmail.com>
2026-08-06 21:49:05 +0300

    Add VIBRATE permission and Compose resources dependency
    - Add `android.permission.VIBRATE` permission to `AndroidManifest.xml`.
    - Add `libs.compose.components.resources` dependency to `shared/build.gradle.kts`.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -47,6 +47,7 @@ kotlin {
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
             implementation(libs.compose.material3)
+            implementation(libs.compose.components.resources)
             implementation(libs.compose.uiToolingPreview)
 
             implementation(libs.kotlinx.coroutines.core)
