0b629dbcd05be1fdb81c11c16b2b4e8db01d8a10
riramzy <ramzyi081@gmail.com>
2026-08-15 23:20:13 +0300

    Add `PillFlowLogCard` component
    - Implement `PillFlowLogCard` composable component to display patient medication activity logs styled by `ComplianceStatus`.
    - Add light and dark theme preview composables.

---
+++ shared/src/commonMain/kotlin/com/riramzy/pillfllow/ui/components/settings/PillFlowUserProfileCard.kt
@@ -0,0 +1,155 @@
+package com.riramzy.pillfllow.ui.components.settings
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_YES
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.riramzy.pillfllow.ui.components.custom.PillFlowButton
+import com.riramzy.pillfllow.ui.theme.PillFlowTheme
+import com.riramzy.pillfllow.utils.IndicatorColor
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.vectorResource
+import pillfllow.shared.generated.resources.Res
+import pillfllow.shared.generated.resources.avatar1
+import pillfllow.shared.generated.resources.edit
+
+@Composable
+fun PillFlowUserProfileCard(
+    userName: String = "Ramzy Habel",
+    userEmail: String = "ramzyhabel@pillflow.com",
+    avatar: DrawableResource = Res.drawable.avatar1,
+    onEditClick: () -> Unit = {},
+    onSignOutClick: () -> Unit = {},
+    modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer.copy(0.5f),
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        ),
+        shape = RoundedCornerShape(25.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(50.dp)
+                            .background(
+                                color = MaterialTheme.colorScheme.primaryContainer,
+                                shape = CircleShape
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Image(
+                            painter = painterResource(avatar),
+                            contentDescription = "Avatar",
+                            modifier = Modifier
+                                .size(35.dp)
+                        )
+                    }
+
+                    Column {
+                        Text(
+                            text = userName,
+                            style = MaterialTheme.typography.bodySmall,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+
+                        Text(
+                            text = userEmail,
+                            style = MaterialTheme.typography.bodySmall,
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Normal
+                        )
+                    }
+                }
+
+                IconButton(
+                    onClick = { onEditClick() },
+                    modifier = Modifier
+                        .size(24.dp),
+                    colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        contentColor = MaterialTheme.colorScheme.primary
+                    )
+                ) {
+                    Icon(
+                        imageVector = vectorResource(Res.drawable.edit),
+                        contentDescription = "Edit",
+                        modifier = Modifier
+                            .size(10.dp),
+                    )
+                }
+            }
+
+            PillFlowButton(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = "Sign Out",
+                onClick = { onSignOutClick() },
+                customColor = IndicatorColor.RED_CONTAINER.color,
+                customTextColor = IndicatorColor.RED.color
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PillFlowUserProfileCardPreview() {
+    PillFlowTheme {
+        PillFlowUserProfileCard(modifier = Modifier.padding(15.dp))
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES, showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+fun PillFlowUserProfileCardPreviewDark() {
+    PillFlowTheme {
+        PillFlowUserProfileCard(modifier = Modifier.padding(15.dp))
+    }
+}
