ca63018a6349106d25eeef6f01e5d4798c18b911
riramzy <ramzyi081@gmail.com>
2026-08-19 19:32:21 +0300

    Add Koin Compose ViewModel and Android dependencies
    - Define `koin-compose-viewmodel` and `koin-android` dependencies in `gradle/libs.versions.toml`.
    - Add `koin-compose-viewmodel` to `commonMain` and `koin-android` to `androidMain` dependencies in `shared/build.gradle.kts`.

---
+++ shared/src/androidMain/kotlin/com/riramzy/pillfllow/di/PlatformModule.android.kt
@@ -0,0 +1,14 @@
+package com.riramzy.pillfllow.di
+
+import com.riramzy.pillfllow.data.local.database.PillFlowDatabase
+import com.riramzy.pillfllow.data.local.database.getDatabaseBuilder
+import com.riramzy.pillfllow.data.local.database.getRoomDatabase
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+actual val platformModule: Module = module {
+    single<PillFlowDatabase> {
+        val dbBuilder = getDatabaseBuilder(context = get())
+        getRoomDatabase(dbBuilder)
+    }
+}
